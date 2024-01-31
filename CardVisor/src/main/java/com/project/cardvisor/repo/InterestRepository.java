a7bc97d66468e57db154ce7baad1cafa041b4f5e
Bang Yongsu <yongsu9630@gmail.com>
2024-01-31 09:56:27 +0900

    - interest data

---
+++ CardVisor/src/main/java/com/project/cardvisor/repo/InterestRepository.java
@@ -0,0 +1,9 @@
+package com.project.cardvisor.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.project.cardvisor.vo.InterestVO;
+
+public interface InterestRepository extends CrudRepository<InterestVO, Integer>{
+
+}
