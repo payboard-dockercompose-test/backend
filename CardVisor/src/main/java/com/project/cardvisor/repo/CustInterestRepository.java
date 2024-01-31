c57f9561920452f1243251009ac5a1bc9ee0f44a
purbluue <143508656+purbluue@users.noreply.github.com>
2024-01-31 15:46:00 +0900

    - custinterest data

---
+++ CardVisor/src/main/java/com/project/cardvisor/repo/CustInterestRepository.java
@@ -0,0 +1,9 @@
+package com.project.cardvisor.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.project.cardvisor.vo.CustInterestVO;
+
+public interface CustInterestRepository extends CrudRepository<CustInterestVO, CustInterestVO>{
+
+}
