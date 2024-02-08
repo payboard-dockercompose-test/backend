45eca3c2a7a78f9250828fbaf4317c2ff716be97
DESKTOP-3O6KIK7\User <eunkyung211@naver.com>
2024-02-08 12:45:47 +0900

    - international 로직 추가

--- CardVisor/src/main/java/com/project/cardvisor/repo/PaymentRepository.java
+++ CardVisor/src/main/java/com/project/cardvisor/repo/PaymentRepository.java
@@ -1,7 +1,11 @@
 package com.project.cardvisor.repo;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.project.cardvisor.vo.PaymentsVO;
 
@@ -32,5 +36,40 @@ public interface PaymentRepository extends CrudRepository<PaymentsVO, String>{
 			+ "WHERE currency_code != 'KRW' and pay_date >= DATE_FORMAT(NOW() - INTERVAL 1 MONTH + INTERVAL 1 DAY, '%Y-%m-01 00:00:00') "
 			+ "AND pay_date <= DATE_FORMAT(NOW() - INTERVAL 1 MONTH , '%Y-%m-%d %H:%i:%s')",nativeQuery = true)
 	 Long AbroadLastMonthTotalAmountPayments();
+	
+	
+	/////// 은경 ///////
+	//해외 토탈 결제 금액
+	@Query("select sum(p.pay_amount) from PaymentsVO p where p.currency_code != 'KRW'")
+	Long selectTotalOverseasPayment();
+	
+	//전년 월 대비 올해 월 증감 (+/-)
+	@Query(value = "select thisYear.currentSum - lastYear.lastSum"
+				+ " from"
+				+ " ("
+					+ " SELECT SUM(p.pay_amount) currentSum"
+					+ " FROM payments p"
+					+ " WHERE p.CURRENCY_CODE != 'KRW'"
+					+ " AND YEAR(p.pay_date) = YEAR(CURRENT_DATE)"
+					+ " AND MONTH(p.pay_date) = :month"
+				+ " ) thisYear,"
+				+ " ("
+					+ " SELECT SUM(p.pay_amount) lastSum"
+					+ " FROM payments p"
+					+ " WHERE p.CURRENCY_CODE != 'KRW'"
+					+ " AND YEAR(p.pay_date) = YEAR(CURRENT_DATE) - 1"
+					+ " AND MONTH(p.pay_date) = :month"
+				+ " ) lastYear", nativeQuery = true)
+	Long selectDiffPaymentThisYearAndLastYear(@Param("month") int month);
 
+	//건수가 제일 많은 나라 (순위 리스트업)
+	@Query(value ="SELECT COUNT(P.CURRENCY_CODE), P.CURRENCY_CODE, C.CURRENCY_NATION"
+			+ " FROM PAYMENTS P"
+			+ " JOIN CURRENCY C ON (P.CURRENCY_CODE = C.CURRENCY_CODE)"
+			+ " WHERE P.CURRENCY_CODE != 'KRW'"
+			+ " AND YEAR(P.PAY_DATE) = YEAR(CURRENT_DATE)"
+			+ " GROUP BY P.CURRENCY_CODE, C.CURRENCY_NATION"
+			+ " ORDER BY COUNT(P.CURRENCY_CODE) DESC", nativeQuery = true)
+	List<Map<String, Object>> selectHighestOrderPayment();
+	
 }
