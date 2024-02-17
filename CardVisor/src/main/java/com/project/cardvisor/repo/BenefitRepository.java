53a6ca68545753c97a9c64ea740bf6ce321af42f
Hwasoo-Jeon <royallhs@naver.com>
2024-02-18 06:28:18 +0900

    hsjeon chart 필터 추가 완료

--- CardVisor/src/main/java/com/project/cardvisor/repo/BenefitRepository.java
+++ CardVisor/src/main/java/com/project/cardvisor/repo/BenefitRepository.java
@@ -1,5 +1,6 @@
 package com.project.cardvisor.repo;
 
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -38,6 +39,19 @@ public interface BenefitRepository extends CrudRepository<BenefitVO, Integer>{
 			+ "ORDER BY b.mcc_code asc, sum(p.benefit_amount) desc, b.benefit_id asc", nativeQuery = true)
 	List<Tuple> findByMCC();
 	
+	@Query(value = "select b.mcc_code, m.ctg_name, b.benefit_detail, sum(p.benefit_amount) benefit_amount_sum, b.benefit_id,  b.benefit_pct, b.interest_id "
+			+ "from payments p  "
+			+ "join card_reg_info cri on p.reg_id = cri.reg_id "
+			+ "join card_benefit cb on cb.card_type = cri.card_type "
+			+ "join benefit b on cb.benefit_id = b.benefit_id "
+			+ "JOIN mcc m ON b.mcc_code = m.mcc_code "
+			+ "WHERE DATE_FORMAT(p.pay_date, '%Y-%m') LIKE :date "
+			+ "AND p.benefit_amount > 0 "
+			+ "and b.benefit_id = p.applied_benefit_id "
+			+ "group BY b.benefit_id "
+			+ "ORDER BY b.mcc_code asc, sum(p.benefit_amount) desc, b.benefit_id asc", nativeQuery = true)
+	List<Tuple> findByMCCWithDate(@Param("date") String date);
+	
 	@Query(value = "SELECT DISTINCT(m.ctg_name) "
 			+ "FROM payments p join mcc m "
 			+ "on p.mcc_code = m.mcc_code "
@@ -54,9 +68,10 @@ public interface BenefitRepository extends CrudRepository<BenefitVO, Integer>{
 			+ "LEFT JOIN payments p "
 			+ "ON b.benefit_id = p.applied_benefit_id "
 			+ "WHERE b.mcc_code = :mcc_code "
+			+ "AND (:date is null or DATE_FORMAT(p.pay_date, '%Y-%m') LIKE :date) "
 			+ "GROUP BY b.benefit_id "
 			+ "ORDER BY benefit_id ASC", nativeQuery = true)
-	List<Map<String, Object>> benefitInfoAndCalData(@Param("mcc_code") String mcc_code);
+	List<Map<String, Object>> benefitInfoAndCalData(@Param("mcc_code") String mcc_code, @Param("date") String date);
 	
 	@Query(value = "select b.benefit_id, count(cb.card_type) related_card_cnt, AVG(cl.card_annual_fee) avg_annual_fee "
 			+ "from benefit b "
@@ -82,6 +97,7 @@ public interface BenefitRepository extends CrudRepository<BenefitVO, Integer>{
 			+ "from payments p "
 			+ "join card_reg_info cri on p.reg_id = cri.reg_id "
 			+ "WHERE cri.card_type =:card_type "
+			+ "AND (:date is null or DATE_FORMAT(p.pay_date, '%Y-%m') LIKE :date) "
 			+ "and p.applied_benefit_id =:applied_benefit_id", nativeQuery = true)
-	Map<String, Object> cardComparison(@Param("card_type") int card_type, @Param("applied_benefit_id") int applied_benefit_id);
+	Map<String, Object> cardComparison(@Param("card_type") int card_type, @Param("applied_benefit_id") int applied_benefit_id, @Param("date") String date);
 }
