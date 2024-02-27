5e0a3ac4374196555f7b95c45ab1d74f44a75403
Jeon Hwasoo <royallhs@naver.com>
2024-02-27 12:14:54 +0900

    Merge remote-tracking branch 'origin/main' into hwasoo

--- CardVisor/src/main/java/com/project/cardvisor/config/WebConfig.java
+++ CardVisor/src/main/java/com/project/cardvisor/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-						.allowedOrigins("https://cardvisor.azurewebsites.net","http://localhost:3000")
+						.allowedOrigins("https://cardvisor.azurewebsites.net", "http://localhost:3000")
 						.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
 						.allowedHeaders("*")
 						.allowCredentials(true)
