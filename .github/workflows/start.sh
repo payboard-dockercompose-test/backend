fff4c7945c8ca7804668ea83a048ca5258366df4
Hwasoo-Jeon <134925813+Hwasoo-Jeon@users.noreply.github.com>
2024-02-28 01:56:55 +0900

    Update start.sh

--- .github/workflows/start.sh
+++ .github/workflows/start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Start Spring Boot application
-# nohup java -jar /app/*.jar > /app/app.log &
+nohup java -jar /app/*.jar > /app/app.log &
 
 # Start Apache in the foreground
-# /usr/sbin/apache2ctl -D FOREGROUND
+/usr/sbin/apache2ctl -D FOREGROUND
 
-java -jar /app/*.jar > /app/app.log
-service apache2 start
+# java -jar /app/*.jar > /app/app.log
+# service apache2 start
