430f0fbd77d89fe37b4e8ba570507653aa2564e6
Hwasoo-Jeon <134925813+Hwasoo-Jeon@users.noreply.github.com>
2024-02-28 09:29:59 +0900

    Update start.sh

--- .github/workflows/start.sh
+++ .github/workflows/start.sh
@@ -3,7 +3,7 @@
 # nohup java -jar /app/*.jar > /app/app.log &
 
 # Start Apache in the foreground
-# /usr/sbin/apache2ctl -D FOREGROUND
+/usr/sbin/apache2ctl -D FOREGROUND
 
 # java -jar /app/*.jar > /app/app.log
-service apache2 start
+# service apache2 start
